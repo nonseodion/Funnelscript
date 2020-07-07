e415022929cda9017776400b80761a539f72a948
nonseodion <nonseifebhor@gmail.com>
2020-07-07 21:23:45 +0100

    updated step 4

--- offer-generator.js
+++ offer-generator.js
@@ -8,9 +8,12 @@ const cancel = document.querySelectorAll(".offer .cancel");
 const forms = Array.from(document.querySelectorAll(".offer form"));
 const checkboxes = document.querySelectorAll("#step3 input[type=checkbox]");
 const chosenOfferings = document.querySelector(".chosen-offerings");
-let removeBonus = document.querySelector(".offer .step4 .remove-bonus");
+let removeBonus = document.querySelectorAll(".offer .step4 .remove-bonus");
 const addBonus = document.querySelector(".offer .step4 .add-bonus");
 const bonusTemplate = document.querySelector(".offer .bonus");
+let noOfOffers = document.querySelector(".status .no-of-offers p:nth-child(2)");
+let offerStrength = "Empty";
+
 
 let prices = document.querySelectorAll(".chosen-offerings input[placeholder=Price]");
 
@@ -61,12 +64,6 @@ cancel.forEach(n => n.addEventListener("click", previousStep));
 function previousStep(){
     grandParent = this.parentNode.parentNode;
     grandParent.reset();
-    
-    
-    // const index = forms.indexOf(grandParent);
-    // if (index == 0) return;
-    // forms[index].classList.add("no-display");
-    // forms[index-1].classList.remove("no-display");
 }
 
 
@@ -104,7 +101,7 @@ function updateStatus(){
     let totalPrice = prices.reduce((total, price) => total + price);
     let offers = chosenOfferings.querySelectorAll("input[type=text]").length - 
                     chosenOfferings.querySelectorAll(".no-display").length;
-    let noOfOffers = document.querySelector(".status .no-of-offers p:nth-child(2)");
+    
     let price = document.querySelector(".status .price p:nth-child(2)");
     noOfOffers.textContent = offers;
     price.textContent = "$ "+ totalPrice;
@@ -114,15 +111,19 @@ function updateStatus(){
     
     if(offers >= 7){
         statusList[0].classList.remove("no-display");
+        offerStrength = statusList[0].classList[0];
     }
     else if(offers >= 5){
         statusList[1].classList.remove("no-display");
+        offerStrength = statusList[1].classList[0];
     }
     else if(offers >= 3){
         statusList[2].classList.remove("no-display");
+        offerStrength = statusList[2].classList[0];
     }
     else{
         statusList[3].classList.remove("no-display");
+        offerStrength = statusList[3].classList[0];
     }
 }
 
@@ -130,17 +131,27 @@ function updateStatus(){
 //addBonuses
 addBonus.addEventListener("click", add_bonus);
 function add_bonus(){
-    let bonus = bonusTemplate.cloneNode(true);
-    bonus.classList.remove("no-display");
-    let bonuses = document.querySelector(".step4 .tab-body .bonuses");
-    bonuses.appendChild(bonus);
-    bonus.querySelector(".remove-bonus").addEventListener("click", remove_bonus);
+    let hiddenBonusArray = document.querySelectorAll(".step4 .tab-body .bonuses .bonus.no-display");
+    let displayedBonuses = checkDisplayedBonuses(hiddenBonusArray);
+
+    //hide addBonus button
+    if(Number(noOfOffers.textContent) <= displayedBonuses+1) addBonus.classList.add("no-display");
+    hiddenBonusArray[0].classList.remove("no-display");
 }
 
 //remove Bonus
-removeBonus.addEventListener("click", remove_bonus);
+removeBonus.forEach(bonus => bonus.addEventListener("click", remove_bonus));
 function remove_bonus(){
-    console.log("me");
     let bonus = this.parentNode;
-    bonus.parentNode.removeChild(bonus);
+    bonus.classList.add("no-display");
+
+    //show addBonus button
+    let hiddenBonusArray = document.querySelectorAll(".step4 .tab-body .bonuses .bonus.no-display");
+    let displayedBonuses = checkDisplayedBonuses(hiddenBonusArray);
+    if(Number(noOfOffers.textContent) > displayedBonuses) addBonus.classList.remove("no-display");
+}
+
+function checkDisplayedBonuses(hiddenBonusArray){
+    let bonusArray = document.querySelectorAll(".step4 .tab-body .bonuses .bonus");
+    return bonusArray.length - hiddenBonusArray.length;
 }
